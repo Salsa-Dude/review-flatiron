9ddd0f53b5796ca985ceca8cf40379d3a3fb44ae
Joseph Arias <arias.joseph.a@gmail.com>
2019-04-15 21:43:19 -0400

    mapping database to objects

---
+++ Mod-1/ORM/mapping-database-to-objects.rb
@@ -0,0 +1,135 @@
+
+class Student
+  attr_accessor :id, :name, :grade
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  # This is a class method that accepts a row from the database as an argument.
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  # This is a class method that accepts a name of a student.
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  # we are returning everything in the database.
+  def self.all
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db
+    end
+  end
+
+  # should return an array of all the students in grade 9.
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+          SELECT *
+          FROM students
+          WHERE students.grade = 9
+     SQL
+
+      DB[:conn].execute(sql).map do |row|
+       self.new_from_db(row)
+    end
+  end
+
+  # This method should return an array of all the students below 12th grade.
+  def self.students_below_12th_grade
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE students.grade < 12
+    SQL
+
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  # This is a class method that takes in an argument of the number of students from grade 10 to select.
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  # This should return the first student that is in grade 10.
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+# This method should return an array of all students for grade X
+def self.all_students_in_grade_X(grade)
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    ORDER BY students.id
+  SQL
+
+  DB[:conn].execute(sql, grade).map do |row|
+    self.new_from_db(row)
+  end
+end
+
+
+end
