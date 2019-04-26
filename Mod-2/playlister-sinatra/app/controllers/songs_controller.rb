19c809d5e40c0727552f8ebf72bbf805c34046b9
Joseph Arias <arias.joseph.a@gmail.com>
2019-04-25 22:31:01 -0400

    playlister-sinatra

--- Mod-2/playlister-sinatra/app/controllers/songs_controller.rb
+++ Mod-2/playlister-sinatra/app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
 
+# require 'rack-flash'
+
 class SongsController < ApplicationController
 
   get '/songs' do
@@ -6,10 +8,37 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+   erb :'/songs/new'
+ end
+
+ post '/songs' do
+    @song = Song.create(name: params["name"])
+    @song.artist = Artist.find_or_create_by(name: params["artist_name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    redirect("/songs/#{@song.slug}")
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-
     erb :'songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :"songs/edit"
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    redirect("/songs/#{@song.slug}")
   end
 
 
