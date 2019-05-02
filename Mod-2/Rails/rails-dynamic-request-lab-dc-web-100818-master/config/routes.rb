3d0d2cd003237ab73bf8234b2e38b8b1e4c7a33d
Salsa-Dude <arias.joseph.a@gmail.com>
2019-05-02 08:22:01 -0400

    validations in controller action

--- Mod-2/Rails/rails-dynamic-request-lab-dc-web-100818-master/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :students, only: :index
-
-  get "students/:id", to: 'students#show'
-end
---
+++ Mod-2/Rails/validations-in-controller-actions-rails-lab-dc-web-100818-master/app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
+
+  def show
+    set_post!
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    set_post!
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    set_post!
+  end
+
+  def update
+    if @post.update(post_params)
+     redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:category, :content, :title)
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
+  end
+end
