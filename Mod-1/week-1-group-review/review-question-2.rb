1e10539bc38c1799128c0217f4eedec7c1613af6
Salsa-Dude <arias.joseph.a@gmail.com>
2019-05-09 09:17:34 -0400

    review-question 2 and 3

--- Mod-1/week-1-group-review/review-question-2.rb
+++ Mod-1/week-1-group-review/review-question-2.rb
@@ -1,31 +1,51 @@
 # Finish the implementation of the Car class so it has the functionality described below
+require 'pry'
 
 class Car
 
+  @@all = []
 
+  attr_accessor :make, :model
+  def initialize(make, model)
+    @make = make
+    @model = model
+    self.class.all << self
+  end
 
+  def self.drive
+    "VROOOOOOOOOOOOM!"
+  end
+
+  def self.all
+    @@all
+  end
 end
 
 volvo_lightning = Car.new("Volvo", "Lightning")
 yugo = Car.new("Zastava", "Yugo")
 lada = Car.new("AvtoVAZ", "Lada")
 
-volvo_lightning.make
+p volvo_lightning
+p yugo
+p lada
+#
+# volvo_lightning.make
 #=> "Volvo"
-volvo_lightning.model
+# volvo_lightning.model
 #=> "Lightning"
 
-Car.drive
+p Car.drive
 # => "VROOOOOOOOOOOOM!"
 
-Car.all
+p Car.all
 #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
-BONUS:
+# BONUS:
 
-volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
+# volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
 
-volvo_lightning.make
+# volvo_lightning.make
 #=> "Volvo"
-volvo_lightning.model
+# volvo_lightning.model
 #=> "Lightning"
+p volvo_lightning.make
--- Mod-1/week-1-group-review/review-question-3.rb
+++ Mod-1/week-1-group-review/review-question-3.rb
@@ -1,22 +1,85 @@
+require 'pry'
+
 # begin to build a simple program that models Instagram
 # you should have a User class, a Photo class and a comment class
 
 
+class Photo
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def user=(user_name)
+    @user = user_name
+  end
+
+  def user
+    @user
+  end
+
+  def self.all
+    @@all
+  end
+
+  def comments
+    Comment.all.select {|comment| comment.photo === self}
+  end
+
+  def make_comment(text)
+    Comment.new(text, self)
+  end
+end
+
+class User
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def photos
+    Photo.all.select {|pic| pic.user == self}
+  end
+end
+
+class Comment
+  attr_accessor :text, :photo
+  @@all = []
+
+  def initialize(text, photo)
+    @text = text
+    @photo = photo
+    @all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
+
 sandwich_photo = Photo.new
 sophie = User.new("Sophie")
+p sophie.name
 sandwich_photo.user = sophie
-sandwich_photo.user.name
+p sandwich_photo.user.name
+sandwich_photo
+
 # => "Sophie"
-sophie.photos
+p sophie.photos
 # => [#<Photo:0x00007fae2880b370>]
 
 
-sandwich_photo.comments
+# sandwich_photo.comments
 # => []
 
-sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
-sandwich_photo.comments
+# sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
+# sandwich_photo.comments
 # => [#<Comment:0x00007fae28043700>]
 
-Comment.all
+# Comment.all
 #=> [#<Comment:0x00007fae28043700>]
